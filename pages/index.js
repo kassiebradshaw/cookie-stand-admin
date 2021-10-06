d34fb1e7541132f0227338f0dd0fb965db3030f2
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-10-05 21:23:42 -0700

    Added contexts and external CookieStandAdmin component, trying to get things working

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import ReportTable from '../components/ReportTable'
-import CreateForm from '../components/CreateForm'
 import { hours } from '../data'
+import { useAuth } from '../contexts/auth'
 
-export default function CookieStandAdmin() {
+export default function Home() {
+
+  const { user, login, logout } = useAuth();
 
   const [reports, setAllStores] = useState([])
 
@@ -34,20 +35,6 @@ export default function CookieStandAdmin() {
     return reports[reports.length -1]
   }
 
-  // function calcTotals() {
-  //   let footerTotals = [];
-  //   let grandTotal = 0;
-  //   for (let i = 0; i < hours.length; i++) {
-  //     let hourTotal = 0;
-  //     for (let j=0; j < reports.length; j++) {
-  //       hourTotal += reports[j].hourly_sales[i];
-  //     }
-  //     footerTotals.push(hourTotal);
-  //     grandTotal += hourTotal;
-  //   }
-  //   return footerTotals, grandTotal
-  // }
-
   return (
     <div>
       <Head>
@@ -57,41 +44,11 @@ export default function CookieStandAdmin() {
 
       <Header />
 
-      <main>
-        <CreateForm onCreate={createStoreHandler}/>
-          
-        { reports[0] ? <ReportTable 
-          hours={hours}
-          reports={reports} /> : <h2 className="m-5 text-center">No Cookie Stands Available</h2>
-        }
-        
-      </main>
+      {user ? 
+        <CookieStandAdmin /> :
+        <h2>Need to log in</h2>}
 
       <Footer reports={reports.length}/>
-    
-
-      {/* <main>           
-          <form onSubmit={ createStoreHandler } className="flex p-6 mx-20 my-5 bg-green-300 rounded-md">
-            <div className="flex items-center mx-auto text-center">
-              <label className="text-xl text-center">Create Cookie Stand</label>            
-            </div>
-            <div className="flex items-center">
-              <label className="">Location: <input type="text" name="location" className=""></input></label>  
-              <label for="minCustomers">Minimum Customers per Hour</label>
-              <input name="minCustomers" type="number" className="" />
-              <label for="maxCustomers">Maximum Customers per Hour</label>
-              <input name="maxCustomers" type="number" className="" />
-              <label for="avgCookies">Average Cookies per Sale</label>
-              <input name="avgCookies" type="number" className=""/>
-              <button className="px-10 py-4 m-2 text-xs bg-green-500">Create</button>
-            </div>
-          </form>
-            
-          <div className="w-1/2 m-3 mx-auto">
-            <p className="mx-auto mt-5 mb-5 text-center">Report Table Coming Soon...</p>
-            <p className="mx-auto mt-5 mb-5 font-mono text-center">{ JSON.stringify(getLastStore()) }</p>
-          </div>
-      </main> */}
 
     </div>
   )
